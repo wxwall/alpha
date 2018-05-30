2a6cedff951826da8e6db6e39a058ccd29f4c14b
asiainfo <yuanchi@asiainfo.com>
2018-05-30 09:27:36 +0800

    Apollo代码提交-yuanchi

---
+++ config/src/main/java/com/asiainfo/config/common/ApolloMDAConfig.java
@@ -0,0 +1,22 @@
+package com.asiainfo.config.common;
+
+import com.asiainfo.config.busi.ApolloMDA;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wuxiaowei on 2018/5/18
+ */
+@Configuration
+@EnableApolloConfig
+public class ApolloMDAConfig
+{
+
+    @Bean
+    public ApolloMDA javaConfigBean() {
+
+        return new ApolloMDA();
+
+    }
+}
