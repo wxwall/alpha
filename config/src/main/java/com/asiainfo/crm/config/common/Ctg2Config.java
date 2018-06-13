08d5cfa4ba74862003fdc08c8b8a5360cabd4187
huangshun <huangshun@asiainfo.com>
2018-06-13 15:10:54 +0800

    数据库组件打开，数据源调整

--- config/src/main/java/com/asiainfo/crm/config/common/Ctg2Config.java
+++ config/src/main/java/com/asiainfo/crm/config/common/Ctg2Config.java
@@ -1,13 +1,14 @@
 package com.asiainfo.crm.config.common;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 主要是扫描technology-dbua.jar的包。这个需要组件调整报名统一
  **/
 @Configuration
-/*@ComponentScan(
+@ComponentScan(
         basePackages = {"com.ai.*"}
-)*/
+)
 public class Ctg2Config {
 }
