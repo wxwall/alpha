1cc96b044f1564638797c2137abc84870ea012aa
huangshun <huangshun@asiainfo.com>
2018-07-11 11:44:13 +0800

    集成udal启动器并测试通过

--- service-web/src/main/java/com/asiainfo/crm/ServiceApplication.java
+++ service-web/src/main/java/com/asiainfo/crm/ServiceApplication.java
@@ -1,7 +1,6 @@
 package com.asiainfo.crm;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
